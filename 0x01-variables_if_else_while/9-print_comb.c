3d660eb2696107c92e638b52c55c784d3f84b422
anaruzz <laabidigh@gmail.com>
2019-09-19 17:21:50 +0100

    fml

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,10 +7,15 @@
 int main(void)
 {
 int i;
-for (i = '0' ; i <= '9' ; i++)
-putchar(i % 10 + '0');
-for (i = 'A' ; i <= 'F' ; i++)
+for (i = '0' ; i <= '9'; i++)
+{
 putchar(i);
+if (i != '9')
+{
+putchar (',');
+putchar (' ');
+}
+}
 putchar("\n");
 return (0);
 }
