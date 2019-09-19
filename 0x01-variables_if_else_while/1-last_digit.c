4f0017009830d4e2cec33ce9c3c47b7a0ca61bf1
anaruzz <laabidigh@gmail.com>
2019-09-19 14:07:33 +0100

    commit 4

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,6 +19,7 @@ else
 if (a > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, a);
 else
+if (a < 6)   
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 return (0);
 }
