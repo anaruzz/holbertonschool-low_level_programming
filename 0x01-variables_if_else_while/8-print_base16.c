07383aff574beef5e0ee55a8e90e1eacf6eb2323
anaruzz <laabidigh@gmail.com>
2019-09-19 17:03:51 +0100

    commit 12

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int i, j;
+for (i = '0' ; i <= '9' ; i++)
+putchar(i);
+for (j = 'A' ; j <= 'F' ; j++)
+putchar(j);
+putchar ('\n');
+return (0);
+}
