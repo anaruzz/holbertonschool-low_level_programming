06584b1698d520521641f3044be638d5ed462694
anaruzz <laabidigh@gmail.com>
2019-09-24 11:36:59 +0100

    whatever

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,8 +11,7 @@ if (n > 0)
 _putchar('+');
 return (1);
 }
-else
-if (n == 0)
+else if (n == 0)
 {
 _putchar('0');
 return (0);
@@ -20,6 +19,6 @@ return (0);
 else
 {
 _putchar('-');
-return (9);
+return (-1);
 }
 }
