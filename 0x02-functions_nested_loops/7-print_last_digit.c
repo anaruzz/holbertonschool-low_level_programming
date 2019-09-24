ed734b1f09b1a2ea6d64202aebabcb4683ba8128
anaruzz <laabidigh@gmail.com>
2019-09-24 12:57:52 +0100

    commit 8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,10 +3,12 @@
  * print_last_digit - entry point
  * @i : number to print last digit
  * Return: int
- */
+*/
 int print_last_digit(int i)
 {
 int x = i % 10;
-_putchar(x);
+if (x < 0)
+x = -x;
+_putchar(x + '0');
 return (x);
 }
