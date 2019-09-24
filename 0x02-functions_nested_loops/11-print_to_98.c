1eaed839581fe3f985cf22e3c88c67b8af962d90
anaruzz <laabidigh@gmail.com>
2019-09-24 17:14:01 +0100

    finally

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,22 +11,36 @@ int i;
 i = n;
 if (i < 99)
 {
-i = n;
 while (i < 99)
 {
+if (i != 98)
+{
 printf("%d, ", i);
-
+i++;
+}
+else
+{   
+printf("%d", i);
 i++;
 }
 }
+printf("\n");
+}
 else if (i > 98)
 {
-i = n;
-while (i > 99)
+while (i >= 98)
+{
+if (i != 98)
 {
 printf("%d, ", i);
 i--;
 }
+else
+{
+printf("%d", i);
+i--;
 }
-_putchar('\n');
+}
+printf("\n");
+}
 }
