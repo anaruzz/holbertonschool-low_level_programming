778243d6a9dace2b3f9db7bfa58c6217d199565e
anaruzz <laabidigh@gmail.com>
2019-09-26 15:28:42 +0100

    commit 8

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,25 +9,25 @@ void print_diagonal(int n)
 if (n <= 0)
 _putchar('\n');
 else
-   {
-   int i, j;
-   for (i = 1; i <= n; i++)
-     {
-     for (j = 1; j <= n; j++)
-       {
-       if (i == j)
-          {
-          _putchar('\\');
-          }
-       else
-	 {
-           if (i > j)
-           {
-           _putchar(' ');
-           }
-	 }
-       }
-   _putchar('\n');
-    }
-  }
+{
+int i, j;
+for (i = 1; i <= n; i++)
+{
+for (j = 1; j <= n; j++)
+{
+if (i == j)
+{
+_putchar('\\');
+}
+else
+{
+if (i > j)
+{
+_putchar(' ');
+}
+}
+}
+_putchar('\n');
+}
+}
 }
