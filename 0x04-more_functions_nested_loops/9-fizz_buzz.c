bb657c420a32b4141960120ea5af44257471278f
anaruzz <laabidigh@gmail.com>
2019-09-26 19:03:41 +0100

    commit 10

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,3 +1,29 @@
-#include "holberton.h"
+#include <stdio.h>
 /**
+ *main - print numbers
  *
+ *Return: Always 0.
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+  if( ((i % 3) == 0) &&  ((i % 5) != 0))
+{
+printf("Fizz ");
+}
+  else if (((i % 5) == 0) && ((i % 3) != 0))
+{
+printf("Buzz ");
+}
+else if (((i % 3) == 0) && ((i % 5) == 0))
+{
+printf("FizzBuzz ");
+}
+else
+printf("%d ", i);
+}
+return (0);
+}
+
