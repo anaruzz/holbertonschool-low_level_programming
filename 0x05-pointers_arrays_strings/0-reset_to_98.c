e6619905599042aa1171d36954350d5f65a86f4e
anaruzz <laabidigh@gmail.com>
2019-10-01 09:28:39 +0100

    commit 2

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
  * reset_to_98 - reset int to 98
- * @*n: integer to reset
+ * @n: integer to reset
  *Return: void
  */
 void reset_to_98(int *n)
 {
-  *n = 98;
+*n = 98;
 }
