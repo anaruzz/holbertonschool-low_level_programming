66d20adaed7c350009819c45ebcc25d50834c481
anaruzz <laabidigh@gmail.com>
2019-10-01 09:34:50 +0100

    commit 2

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,3 +1,5 @@
+#ifndef HEADER_FILE
+#define HEADER_FI
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -9,3 +11,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
+#endif
