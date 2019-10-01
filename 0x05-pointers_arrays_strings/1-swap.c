de47fd165d0ef58cfc9286c3f4755a33a0b86879
anaruzz <laabidigh@gmail.com>
2019-10-01 09:37:18 +0100

    commit 3

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
  * swap_int - swap two integers
- * @*a: first int
- * @*b: second int
+ * @a: first int
+ * @b: second int
  * Return: void
  */
 void swap_int(int *a, int *b)
 {
-  int c ;
-  c = *a ;
-  *a = *b;
-  *b = c;
+int c;
+c = *a;
+*a = *b;
+*b = c;
 }
