ba1c1d6f3096e0c7cb18e5603cacdc625e9956b5
anaruzz <laabidigh@gmail.com>
2019-10-01 10:10:32 +0100

    commit 10

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,11 +6,11 @@
  */
 void puts2(char *str)
 {
-int i = 0;
- while ((*(str + i) != '\0') && (i % 2))
+int i;
+for (i = 0; *(str + i) != '\0'; i++)
 {
+if (i % 2 == 0)
 _putchar(*(str + i));
-i++
 }
 _putchar('\n');
 }
