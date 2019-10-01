9b6cd674849c1c9e15b955a238252b7145932308
anaruzz <laabidigh@gmail.com>
2019-10-01 09:41:02 +0100

    commit 5

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
  * _puts - print a string
- * @*str: string to print
+ * @str: string to print
  * Return: void
  */
 void _puts(char *str)
 {
 int i = 0;
-while(*(str + i) != '\0')
+while (*(str + i) != '\0')
 {
 _putchar(*(str + i));
 i++;
