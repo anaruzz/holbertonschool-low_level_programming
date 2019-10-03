6accdef4cec58ed6ea73706d149c5b573cbd287a
anaruzz <laabidigh@gmail.com>
2019-10-03 21:57:23 +0100

    pl

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,13 +10,15 @@ int i, j;
 char s1[] = {'a', 'e', 'o', 't', 'l'};
 char s3[] = {'A', 'E', 'O', 'T', 'L'};
 char s2[] = {'4', '3', '0', '7', '1'};
-for (i = 0 ; s[i]; i++)
+i = 0;
+while (s[i])
 {
-for (j = 0; j < 10; j++)
+for (j = 0; s2[j]; j++)
 {
 if (s[i] == s1[j] || s[i] == s3[j])
 s[i] = s2[j];
 }
+i++;
 }
 return (s);
 }
