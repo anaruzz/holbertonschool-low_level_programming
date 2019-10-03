bb460946ff1b645a694f4b51365d4d80d2abca52
anaruzz <laabidigh@gmail.com>
2019-10-03 17:44:10 +0100

    w

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,18 +7,26 @@
  */
 char *cap_string(char *c)
 {
-int i = 0, j;
+  int i = 0, x, j;
 while (c[i])
 i++;
 j = 0;
 while (j <= i)
 {
-if (c[j - 1] == ' ' || c[j - 1] == '\n' || c[j - 1] == 9 || c[j - 1] == '.')
+if (c[j - 1] == ' ' || c[j - 1] == '\n' || c[j - 1] == '\t' || c[j - 1] == '.')
 {
 if (c[j] >= 97 && c[j] <= 122)
 c[j] = c[j] - 32;
 }
 j++;
+}
+for(j = 0; j<= i; j++)
+{
+if (c[j] == '\t')
+{
+for (x = j; x <= i; x++)
+c[x] = c[x + 1];
+}
 }
 return (c);
 }
