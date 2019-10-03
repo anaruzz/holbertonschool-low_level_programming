8ce1257b1497818d90296b42f16430a3e8589aef
anaruzz <laabidigh@gmail.com>
2019-10-03 19:44:45 +0100

    finally

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,12 +7,12 @@
 char *leet(char *s)
 {
 int i, j;
-char s1[] = {'a', 'e', 'o', 't','l'};
+char s1[] = {'a', 'e', 'o', 't', 'l'};
 char s3[] = {'A', 'E', 'O', 'T', 'L'};
 char s2[] = {'4', '3', '0', '7', '1'};
-for (i= 0 ; s[i]; i++)
+for (i = 0 ; s[i]; i++)
 {
-for (j = 0; s2[j]; j++)
+for (j = 0; j < 10; j++)
 {
 if (s[i] == s1[j] || s[i] == s3[j])
 s[i] = s2[j];
