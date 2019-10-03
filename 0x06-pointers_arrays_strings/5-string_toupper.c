c9f30d205de5bab5aab0165e17e4232a3fcd86ec
anaruzz <laabidigh@gmail.com>
2019-10-03 21:47:43 +0100

    ok

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
  * string_toupper - change lowercase letters
- * 
- * Return: void
+ * @ch: char
+ * Return: char
  */
 char *string_toupper(char *ch)
 {
 int i;
 for (i = 0; ch[i]; i++)
 {
-if(ch[i] >= 'a' && ch[i] <= 'z')
+if (ch[i] >= 'a' && ch[i] <= 'z')
 ch[i] = ch[i] - 32;
 }
 return (ch);
