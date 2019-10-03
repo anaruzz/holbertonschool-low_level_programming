19dc02ff497d77376c6394a80f4948962242ef88
anaruzz <laabidigh@gmail.com>
2019-10-03 23:54:13 +0100

    commit

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,27 +1,31 @@
 #include "holberton.h"
 /**
- * rot13 - rot13 encoding generator
- * @s: char 
- * Return: char
- */
+* rot13 - rot13 enoding generator *
+* @s: char pointer *
+* Return: return char *
+*/
 char *rot13(char *s)
 {
-int i,j, n = 0;
-char old[] = "ABCDEFGHIJKLMabcdefgihijklmNOPQRSTUVWXYZnopqrstuvwxyz";
-char new[] = "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm";
-for (i = 0; ch[i]; i++)
+int i;
+int j;
+int n = 0;
+char oldalpha[] = "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz";
+char newalpha[] = "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm";
+i = 0;
+j = 0;
+while (s[i] != '\0')
 {
-while (old[j] != && n == 0)
+while (oldalpha[j] != '\0' && n == 0)
 {
-if (s[i] == old[j])
+if (s[i] == oldalpha[j])
 {
-s[i] = new[j];
+s[i] = newalpha[j];
 n = 1;
 }
 j++;
 }
 n = 0;
+i++;
 }
 return (s);
 }
-
