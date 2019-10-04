4b434b9a322e06ba4cfbc5e39d18b45ecf4aa2ba
anaruzz <laabidigh@gmail.com>
2019-10-04 15:39:30 +0100

    commit

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -5,17 +5,17 @@
  * Return: void
  */
 void print_number(int n)
-  { 
+{
 if (n < 0)
 {
-n = (- 1) * n;
+n = (-1) * n;
 _putchar('-');
 }
-    if( n > 9 )
-      { int a = n / 10;
-
-        n -= 10 * a;
-        print_number(a);
-      }
-    _putchar('0'+n);
-  }
+if (n > 9)
+{
+int a = n / 10;
+n -= 10 * a;
+print_number(a);
+}
+_putchar('0' + n);
+}
