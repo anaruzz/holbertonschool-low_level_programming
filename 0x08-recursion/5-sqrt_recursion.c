2ed4a3f67abb7e5f2697d5a05b7ec8fb67d2c0d9
anaruzz <laabidigh@gmail.com>
2019-10-10 10:55:07 +0100

    pld

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,28 +1,29 @@
 #include "holberton.h"
 /**
-* _sqrt_recursion - natural square
+*_sqrt_recursion - natural square
 * @n : int
-* Return : int
+* Return: int
 */
 int _sqrt_recursion(int n)
 {
 if (n < 0)
 return (-1);
-else
-return (test_sqrt(n, n/2));
+else if (n == 1)
+return (1);
+return (test_sqrt(n, n / 2));
 }
 /**
-* test_sqrt- test natural squar
+*test_sqrt - test natural squar
 * @a : int
 * @b : int
-* Return : int
+* Return: int
 */
 int test_sqrt(int a, int b)
 {
-if (b *b == a)
+if (b * b == a)
 return (b);
 else if (b == 1)
 return (-1);
 else
-return (test_sqrt(a+1, b));
+return (test_sqrt(a, b - 1));
 }
