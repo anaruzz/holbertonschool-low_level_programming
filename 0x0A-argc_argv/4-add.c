07c38073876e2d812310990d7609c2f8b426bd56
anaruzz <laabidigh@gmail.com>
2019-10-13 22:16:46 +0100

    now

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,22 +8,18 @@
 */
 int main(int argc, char *argv[])
 {
-if (argc < 1)
-{
-printf("0\n");
-}
 int i, j, x = 0;
 for (i = 1; i < argc; i++)
 {
-x += atoi(argv[i]);
 for (j = 0; argv[i][j]; j++)
 {
 if (argv[i][j] < 48  || argv[i][j] > 57)
 {
-printf("Error");
+printf("Error\n");
 return (1);
 }
 }
+x += atoi(argv[i]);
 }
 printf("%d\n", x);
 return (0);
