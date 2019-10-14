bf006fa5bd8ec137d80497dfac3840d55a0ab210
anaruzz <laabidigh@gmail.com>
2019-10-14 16:29:46 +0100

    whatever

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,16 +9,7 @@
 int main(int argc, char *argv[])
 {
 int i = 0;
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
-if (atoi(argv[1]) <= 0)
-{
-printf("0\n");
-return (1);
-}
+
 int x = atoi(argv[1]);
 if (x >= 25)
 {
