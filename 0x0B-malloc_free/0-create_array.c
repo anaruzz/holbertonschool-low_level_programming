f17de21f6366bf5e723a35e233bf86ba433045ee
anaruzz <laabidigh@gmail.com>
2019-10-16 12:51:25 +0100

    whatever

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,16 +10,11 @@ char *create_array(unsigned int size, char c)
 {
 if (size == 0)
 return (NULL);
-else
-{
-char *t ;
+char *t;
 t = malloc(size * sizeof(char));
 unsigned int i;
 for (i = 0 ; i < size; i++)
-{
 *(t + i) = c;
-}
-}
 if (sizeof(t) == 0)
 return (NULL);
 else
