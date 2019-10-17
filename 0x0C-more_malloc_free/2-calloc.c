2520ed0b3430d4b90cb7a8574ce7a16ed9379ae5
anaruzz <laabidigh@gmail.com>
2019-10-17 21:38:32 +0100

    fml

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,15 +4,14 @@
 #include <limits.h>
 /**
 * _calloc - concatenate two strings
-* @s1: strings
-* @s2: string
-* @n: unsigned int
+* @nmemb: strings
+* @size: int
 * Return: Pointer
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int i;
-char * c;
+char *c;
 if (nmemb <= 0 || size <= 0)
 return (NULL);
 c = malloc(nmemb * size);
