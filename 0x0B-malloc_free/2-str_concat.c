266574b8d74a1de0e5c89d86f8d283063648d66b
anaruzz <laabidigh@gmail.com>
2019-10-18 15:55:45 +0100

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,36 +11,28 @@ char *str_concat(char *s1, char *s2)
 {
 unsigned int j = 0, k = 0, i = 0;
 char *ch;
-if (s1 == NULL)
-{
-s1[0] = '\0';
-}
-else
+if (s1 != NULL)
 {
 while (s1[i] != '\0')
 i++;
 }
-if (s2 == NULL)
-{
-s1[0] = '\0';
-}
-else
+
+if (s2 != NULL)
 {
 while (s2[j] != '\0')
 j++;
 }
-ch = malloc((i + j) *sizeof(char) + 1);
+
+ch = malloc(sizeof(char) * (i + j) + 1);
 if (ch == NULL)
 return (NULL);
 for (k = 0 ; k < i; k++)
 ch[k] = s1[k];
-i = i + j;
-j = 0;
-for (; k < i; k++)
-{
-ch[k] = s2[j];
-j++;
-}
-ch[k + 1] = '\0';
+
+
+for (k = 0; k < j; k++)
+ch[k + i] = s2[j];
+ch[j + i] = '\0';
+
 return (ch);
 }
