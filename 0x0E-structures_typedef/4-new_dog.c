5ae9bd177a70a0b80da5f5c8ebce5b30b5f9fa90
anaruzz <laabidigh@gmail.com>
2019-10-21 23:39:24 +0100

    whatever

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,12 +1,11 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
-* _strdup - copy a string
-* @str: pointer to string
-* Return: pointer
-*/
+ * _strdup - string duplicates
+ * @str: char pointer
+ * Return: pointer to the duplicated string
+ */
 char *_strdup(char *str)
 {
 int i = 0;
@@ -22,32 +21,24 @@ for (i++; i--;)
 ret[i] = *--str;
 return (ret);
 }
-
 /**
- * new_dog - create dog
- * @name: string
- * @age: float
- * @owner: string
- * Return: Void.
+ * new_dog - function of new dog
+ * @name: pointer name
+ * @age: age float
+ * @owner: pointer char
+ * Return: return dog_t.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *d;
-d = malloc(sizeof(dog_t));
-if (d == NULL)
+dog_t *dog = malloc(sizeof(dog_t));
+if (!dog)
 return (NULL);
-d->name = _strdup(name);
-if (d->name == NULL && name)
-{
-free(d);
-return (NULL);
-}
-d->age = age;
-d->owner = _strdup(owner);
-if (d->owner == NULL && owner)
-{
-free(d);
-return (NULL);
-}
-return (d);
+dog->name = _strdup(name);
+if (name && !dog->name)
+return (free(dog), NULL);
+dog->owner = _strdup(owner);
+if (owner && !dog->owner)
+return (free(dog->name), free(dog), NULL);
+dog->age = age;
+return (dog);
 }
