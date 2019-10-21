6e2aceb9dd8db7ba2a54eccc959e732422ced267
anaruzz <laabidigh@gmail.com>
2019-10-21 13:21:16 +0100

    commit 3

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,6 @@ if (d->name != NULL)
 printf("Name: %s\n", d->name);
 else
 printf("Name : (nil)");
-
 if (d->age != 0)
 printf("Age: %f\n", d->age);
 else
@@ -23,6 +22,4 @@ printf("Owner: %s\n", d->owner);
 else
 printf("Owner: (nil)");
 }
-else
-_putchar('\0');
 }
