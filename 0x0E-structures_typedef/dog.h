b108dfedac9c0d9c9980a911496a5c047703c65c
anaruzz <laabidigh@gmail.com>
2019-10-21 12:45:15 +0100

    commit 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - Short description
+* @name: name of the dog
+* @age: age of dog
+* @owner: name of owner
+* Description: Longer description
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+#endif
