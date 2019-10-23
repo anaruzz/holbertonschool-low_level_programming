1e802d021018298e28b875dbbc7c888b59ccbda9
anaruzz <laabidigh@gmail.com>
2019-10-23 19:06:49 +0100

    seriously?

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,11 +11,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 if (size <= 0 || array == NULL || cmp == NULL)
 return (-1);
-int i = 0;
+int i = 0,j;
 while (i <= size)
 {
 i++;
-if(cmp(array[i]) == 1)
+j = cmp(array[i]);
+if(j == 1)
 return (i);
 }
 return (-1);
