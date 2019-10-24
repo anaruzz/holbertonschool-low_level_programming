7e4460c0cacb1feab6a3673967acd45798d71e27
anaruzz <laabidigh@gmail.com>
2019-10-24 14:39:18 +0100

    idk

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - check the code
+* @argc: argument count.
+* @argv: argument vector.
+*
+* Return: Always 0
+*/
+int main (int argc, char **argv)
+{
+int i, bytes;
+char *hexa;
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+bytes = atoi(argv[1]);
+if (bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+hexa = (char *) main;
+for (i = 0; i < bytes; i++)
+{
+printf("%02hhx ", hexa[i]);
+}
+return (0);
+}
