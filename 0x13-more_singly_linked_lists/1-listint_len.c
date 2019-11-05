8c4792f57f881f0482a991721c1ffe099d9795f2
anaruzz <laabidigh@gmail.com>
2019-11-05 15:44:14 +0100

    done

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
- #include "lists.h"
- /**
-  * listint_len - print all the elements of a list
-  * @h: listint_t
-  * Return: number of nodes
-  */
-  size_t listint_len(const listint_t *h)
-  {
-  int i = 0;
-  while (h)
-  {
-  h = h->next;
-  i++;
-  }
-  return (i);
-  }
+#include "lists.h"
+/**
+* listint_len - print all the elements of a list
+* @h: listint_t
+* Return: number of nodes
+*/
+size_t listint_len(const listint_t *h)
+{
+int i = 0;
+while (h)
+{
+h = h->next;
+i++;
+}
+return (i);
+}
