7d04b3cd41160578ddfda6300ea384a50445d039
anaruzz <laabidigh@gmail.com>
2019-11-05 16:52:28 +0100

    task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,9 +10,14 @@ int pop_listint(listint_t **head)
 {
 listint_t *temp;
 int i;
+
+if (*head == NULL)
+return (0);
+
 temp = *head;
 i = temp->n;
 *head = (*head)->next;
 free(temp);
+
 return (i);
 }
