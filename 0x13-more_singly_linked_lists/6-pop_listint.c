f35917f87a6ffe881dadaee044cccf45d3df4469
anaruzz <laabidigh@gmail.com>
2019-11-05 16:50:06 +0100

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+* pop_listint - free a linked list
+* @head: listint_t
+* Return: data of element deleted
+*/
+int pop_listint(listint_t **head)
+{
+listint_t *temp;
+int i;
+temp = *head;
+i = temp->n;
+*head = (*head)->next;
+free(temp);
+return (i);
+}
