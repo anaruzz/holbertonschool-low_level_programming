efdba2558b89b2dfe21ba7ba26fb0769b4f86396
anaruzz <laabidigh@gmail.com>
2019-11-09 02:28:33 +0100

    task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,31 +2,21 @@
 #include <stdlib.h>
 
 /**
- * test - ftest an int
- * @b: pointer
- * Return: int
-*/
-int test (char *b)
-{
-int i = 0, c = 0;
-
-}
-
-/**
  * puis - puisance
- * @b[i]: integer
- *@@i: integer
+ * @x: integer
+ * @y: integer
  * Return: int
 */
-int puis (char *x, int i)
+int puis (int a, int b)
 {
-int a, j;
-a = atoi (x);
-for (; i >=0; i--)
-j *= a;
-return (j);
-}
+int r = a;
+if (b == 0)
+return (1);
 
+for (; b > 1; b--)
+a *= r;
+return (a);
+}
 
 /**
  * binary_to_uint - function that converts a binary to an unsigned int
@@ -35,21 +25,25 @@ return (j);
 */
 unsigned int binary_to_uint(const char *b)
 {
-int len, i, j, s;
+int i, len2, res = 0;
 
-if (b == NULL ||
+if (b == NULL)
 return (0);
 
-for (len = 0; b[len]; len++)
-;
-j = 0;
-s = 0;
-for (i = len + 1; i >= 0; i--)
+i = 0;
+while (b[i])
 {
-if (b[i] == '0')
+if (b[i] != '0' && b[i] != '1')
+return (0);
+i++;
+}
+
+i--;
+for (len2 = i; len2 >= 0; len2--)
 {
-s = s + (puis(b[i], i) * b[i]);
-s *=10;
+if (b[len2] == '1')
+res += puis(2, i - len2);
 }
-}
+
+return (res);
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,7 +11,7 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 unsigned long int bit;
-if (index > 31)
+if  (index > 31)
 return (-1);
 bit = 1 << index;
 *n = *n | bit;
