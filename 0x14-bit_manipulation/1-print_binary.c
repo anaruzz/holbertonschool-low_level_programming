ce86c329d9eb07887d8b374fcb0f7809a05dd926
anaruzz <laabidigh@gmail.com>
2019-11-11 00:38:26 +0100

    Task 1 done

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,21 +1,39 @@
 #include "holberton.h"
+
 /**
-* print_binary - prints the binary rep
-* of a number.
-* @n: unsigned long int.
-* Return: no return.
+* rev - reverse a binary representation
+* @n: unsigned long integer
+* Return: unsigned long integer
+*/
+unsigned long int rev(unsigned long int n)
+{
+unsigned long int r = 0;
+while (n > 0)
+{
+r <<= 1;
+if (n & 1)
+r ^= 1;
+n >>= 1;
+}
+return (r);
+}
+/**
+* print_binary - prints the binary representation of a number.
+* @n: unsigned long int
+* Return: Void
 */
 void print_binary(unsigned long int n)
 {
-if (n >> 0)
-{
-  
-if (n >> 1)
-print_binary(n >> 1);
-_putchar((n & 1) + '0');
-}
-else
-{
+if (n == 0)
 _putchar('0');
+else
+n = rev(n);
+while (n != 0)
+{
+if (n & 1)
+_putchar('1');
+else
+_putchar('0');
+n = n >> 1;
 }
 }
