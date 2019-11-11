33a1687a829c8b2f11a60bb6f4d2975ca1208b63
anaruzz <laabidigh@gmail.com>
2019-11-11 01:49:54 +0100

    task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,19 +1,20 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * set_bit - sets a bit at index to 1
- * @n: pointer to int to bit-set
- * @index: index to set bit at
- * Return: 1 or -1
- */
+* set_bit - sets a bit at index to 1
+* @n: pointer to int to bit-set
+* @index: index to set bit at
+* Return: 1 or -1
+*/
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-  long unsigned long int x;
+long unsigned long int x;
 
-  if (index > 31)
-  return (-1);
+if (index > 31)
+return (-1);
+
 x = 1 << index;
 *n |= x;
-  return (1);
+return (1);
 }
