8d2fedead7a38965a32e5e144d2dc877bf77bad0
anaruzz <laabidigh@gmail.com>
2019-11-12 21:47:53 +0100

    task 1 done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,24 +14,25 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-ssize_t wr, len;
+size_t wr, len;
 
 if (filename == NULL)
 return (-1);
 
-fd = open(filename, O_WRONLY | O_RDONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+fd = open(filename, O_WRONLY | O_RDONLY | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
-if (text_content != NULL)
-{
+if (text_content == NULL)
+return (1);
+
 len = 0;
 while (text_content[len])
 len++;
 wr = write(fd, text_content, len);
-}
-close(fd);
-if (wr == -1)
-return (-1);
 
+
+if (wr == -1 || w != i)
+return (-1);
+close(fd);
 return (1);
 }
