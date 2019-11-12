9686f68e09ca689b0051f81c11bc5449c44e3d61
anaruzz <laabidigh@gmail.com>
2019-11-12 22:45:50 +0100

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,30 +6,31 @@
 #include<errno.h>
 #include <sys/types.h>
 /**
- * create_file - reads a text file and prints it to stdout.
+ * append_text_to_file - reads a text file and prints it to stdout.
  * @filename: file's name.
  * @text_content: number of letters it should read and print.
- * Return: number of characters printed
+ * Return: integer
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 int fd, wr, len;
 
 if (filename == NULL)
 return (-1);
-
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-if (fd == -1)
-return (-1);
 if (text_content == NULL)
 return (1);
+
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
+return (-1);
 
 len = 0;
 while (text_content[len])
 len++;
-
+if (!text_content)
+text_content = "";
 wr = write(fd, text_content, len);
-if (wr == -1 )
+if (wr == -1 || wr != len)
 return (-1);
 close(fd);
 return (1);
