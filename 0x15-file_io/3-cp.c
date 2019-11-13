4c4d0ad32351282da1da20abae02343b4a784f5b
anaruzz <laabidigh@gmail.com>
2019-11-13 02:03:50 +0100

    redoing task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,84 +1,66 @@
-#include "holberton.h"
+#include<stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include<fcntl.h>
+#include<errno.h>
+#include <sys/types.h>
 /**
- * verif - verif
- * @fd: fd
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-void verif(int fd)
+int main(int argc, char **argv)
 {
-
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
+    int fd1, fd2, rd;
+char *buf;
+    if (argc != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
+    buf = malloc(1024 * sizeof(char));
+if (argv[1] == NULL)
+{
+  dprintf(STDERR_FILENO, "Error: Can't read from file %s\n1", argv[1]);
+  exit(98);
 }
-
-/**
- * print - print
- * @r1: r1
- * @buff1: buff1
- * @fd1: fd1
- * @fd2: fd2
- * @argv1: argv1
- * @argv2: argv2
- */
-
-void print(ssize_t r1, char *buff1,
-		int fd1, int fd2, char *argv1, char *argv2)
+fd1 = open(argv[1], O_RDONLY | O_WRONLY, 0777);
+if (fd1 == -1)
 {
-	while ((r1 = read(fd1, buff1, 1024)) > 0)
-	{
-		if ((write(fd2, buff1, r1)) != r1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv2);
-			exit(99);
-		}
-	}
-
-	if (r1 == -1)
-		{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv1);
-		exit(98);
-		}
+  dprintf(STDERR_FILENO, "Error: Can't read from file %s\n2", argv[1]);
+  exit(98);
 }
 
 
-/**
- * main - main function
- * @argc: number of arguments
- * @argv: arguments
- * Return: 0
- */
-
-int main(int argc, char *argv[])
+fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd2 == -1)
 {
+  dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+  exit(99);
+}
 
-	int fd1, fd2, f1, f2;
-	ssize_t r1 = 0;
-	char buff1[1024];
-
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	fd1 = open(argv[1], O_RDONLY);
-	if (fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+while ((rd = read(fd1, buf, 1024)) >= 0)
+{
+  if ((write(fd2, buf, rd)) != rd)
+  		{
+  			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+  			exit(99);
+  		}
+}
+if (rd == -1)
+		{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n3", argv[1]);
 		exit(98);
-	}
-	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	print(r1, buff1, fd1, fd2, argv[1], argv[2]);
-	f1 = close(fd1);
-	f2 = close(fd2);
-	verif(f1);
-	verif(f2);
-	return (0);
+		}
+if (close (fd1) == -1)
+{
+  dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[1]);
+  exit(100);
+}
+if (close(fd2) == -1)
+{
+  dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[2]);
+exit(100);
+}
+    return (0);
 }
