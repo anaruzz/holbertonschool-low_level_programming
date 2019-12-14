d994e290fcf881cd4d68c499ffc88c79bc4b279e
anaruzz <laabidigh@gmail.com>
2019-12-14 15:22:06 +0100

    Task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* dlistint_len - length of a doubly linked list
+* @head: head
+* @n: data
+* Return: number of nodes printed
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *aux;
+aux = (dlistint_t*)malloc(sizeof(dlistint_t));
+if (!aux)
+return (NULL);
+
+aux->n = n;
+aux->prev = NULL;
+aux->next = (*head);
+
+if ((*head) != NULL)
+(*head)->prev = aux;
+
+*head = aux;
+
+return (*head);
+}
