139c9e9ce14424947f9e053b3bcc61eb0ccc2681
anaruzz <laabidigh@gmail.com>
2019-12-18 21:29:14 +0100

    fix 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,41 +1,64 @@
 #include "lists.h"
-#include <stdio.h>
+/**
+ * func - assign
+ * @new: node
+ * @p: node
+ */
+
+void func(dlistint_t *new, dlistint_t *p)
+{
+	new->next = NULL;
+	new->prev = p;
+	p->next = new;
+}
 
 /**
-* insert_dnodeint_at_index - add a node at the begenning of a doubly linked list
-* @h: head
-* @n: data
-* @idx: index
-* Return: number of nodes printed
-*/
+ * insert_dnodeint_at_index - insert node at a given position
+ * @h: head of list
+ * @idx: index
+ * @n: number
+ * Return: address of new node or NULL if it failed
+ */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new, *temp = *h;
-unsigned int i = 0;
-new = (dlistint_t *)malloc(sizeof(dlistint_t));
-if (!new)
-return (NULL);
 
-new->n = n;
-if (idx == 0)
-add_dnodeint(h, n);
+	dlistint_t *p = *h;
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	unsigned int i = 0;
 
-while (i < idx && temp != NULL)
-{
-  temp = temp->next;
-  i++;
-}
-
-if (temp->next == NULL)
-add_dnodeint_end(h, n);
-
-if (i == idx)
-{
-new->next = temp->next;
-new->prev = temp;
-}
-else
-return (NULL);
-
-return (temp);
+	if (new == NULL || h == NULL)
+		return (NULL);
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *h;
+		new->prev = NULL;
+		if (*h != NULL)
+			(*h)->prev = new;
+		*h = new;
+		return (new);
+	}
+	while (p->next != NULL)
+	{
+		if (i == idx)
+			break;
+		p = p->next;
+		i++;
+	}
+	if (p != NULL && i == idx)
+	{
+		new->next = p;
+		new->prev = p->prev;
+		p->prev->next = new;
+		p->prev = new;
+		return (new);
+	}
+	else if (p->next == NULL && i + 1 == idx)
+	{
+		func(new, p);
+		return (new);
+	}
+	free(new);
+	return (NULL);
 }
