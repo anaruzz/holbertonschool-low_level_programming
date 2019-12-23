b2b9323b48ff42fa0ab6beb673b7650b5281a807
anaruzz <laabidigh@gmail.com>
2019-12-24 00:44:20 +0100

    must be right now

--- 0x17-doubly_linked_lists/palaindrome.c
+++ 0x17-doubly_linked_lists/palaindrome.c
@@ -1,10 +1,27 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+/*
+*/
+int pal(int a)
+{
+int b = a, c = 0;
+while (b)
+{
+c = (b % 10) + ( c * 10);
+b = b / 10;
+}
+if (c == a)
+return (1);
+else
+return (0);
+}
+
 /**
 main - largest palaindrome of two 3 digit numbers
 Return: 0
- */
+*/
 
 int main()
 {
@@ -13,11 +30,11 @@ FILE *fp;
 fp = fopen("102-result", "w");
 for (i = 100; i < 1000; i++)
 {
-	for (j = 100; j < 1000; j++)
-	{
-		if ((i * j) > largest)
-		largest = i * j;
-	}
+for (j = 100; j < 1000; j++)
+{
+if ((i * j) > largest && pal(i * j))
+largest = i * j;
+}
 }
 fprintf(fp,"%d", largest);
 fclose(fp);
