03242e346848a474d963a6744d777c2195f7e833
anaruzz <laabidigh@gmail.com>
2019-12-24 00:20:04 +0100

    adv

--- 0x17-doubly_linked_lists/palaindrome.c
+++ 0x17-doubly_linked_lists/palaindrome.c
@@ -1,5 +1,6 @@
 #include "lists.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
 main - largest palaindrome of two 3 digit numbers
 Return: 0
@@ -8,6 +9,8 @@ Return: 0
 int main()
 {
 int i, j, largest = 0;
+FILE *fp;
+fp = fopen("102-result", "w");
 for (i = 100; i < 1000; i++)
 {
 	for (j = 100; j < 1000; j++)
@@ -16,5 +19,7 @@ for (i = 100; i < 1000; i++)
 		largest = i * j;
 	}
 }
-printf("%d\n", largest);
+fprintf(fp,"%d", largest);
+fclose(fp);
+return (0);
 }
