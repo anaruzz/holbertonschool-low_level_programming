0701930b25708fa87afaf1009b75918466b02d38
anaruzz <laabidigh@gmail.com>
2020-01-20 00:00:40 +0100

    Delete

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,33 +1,30 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "hash_tables.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always EXIT_SUCCESS.
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
  */
 void hash_table_delete(hash_table_t *ht)
 {
-  unsigned int size = ht->size, i;
+unsigned long int i = 0;
 hash_node_t *tmp;
+
 if (ht)
 {
-  for (i = 0; i < size; i++)
-  {
-  if (ht->array[i] != NULL)
-  {
-    tmp = ht->array[i];
-    while(tmp)
-    {
-      ht->array[i] = ht->array[i]->next;
+for  (i = 0; i < ht->size; i++)
+{
+if (ht->array[i])
+{
+tmp = ht->array[i];
+while (tmp)
+{
+ht->array[i] = ht->array[i]->next;
 free(tmp);
 tmp = ht->array[i];
-    }
-  }
-  }
-  free(ht->array);
-  free(ht);
+}
+}
+}
+free(ht->array);
+free(ht);
 }
 }
