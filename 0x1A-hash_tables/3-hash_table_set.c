f6adee32c6b5a32cd550ee13d336f308cd5229dd
anaruzz <laabidigh@gmail.com>
2020-01-19 22:54:24 +0100

    Adding a value to the hashtable

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,6 +2,51 @@
 #include <string.h>
 #include <stdio.h>
 #include "hash_tables.h"
+
+/*
+ * node_handler - handle node
+ * @new : new node to add
+ * @index: index of node to be added
+*/
+void node_handler(hash_table_t *ht, hash_node_t *new, unsigned int index)
+{
+hash_node_t *tmp;
+tmp = ht->array[index];
+
+if (ht->array[index] != NULL)
+{
+tmp = ht->array[index];
+while (tmp)
+{
+if (strcmp(tmp->key, new->key) == 0)
+{
+break;
+}
+tmp = tmp->next;
+}
+if (tmp == NULL)
+{
+new->next = ht->array[index];
+ht->array[index] = new;
+}
+else
+{
+free(tmp->value);
+tmp->value = strdup(tmp->value);
+free(new->value);
+free(new->key);
+free(new);
+}
+}
+else
+{
+new->next = NULL;
+ht->array[index] = new;
+}
+}
+
+
+
 /**
 * hash_table_set - add an element to the hash table
 * @ht: hash table
@@ -12,38 +57,22 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int index, size = ht->size;
-hash_node_t *new, *current;
+hash_node_t *new;
 
-if (!key || strlen(key) == 0 || !ht || !value)
+if (!key || strcmp(key, "") == 0 || !ht || !value)
 return (0);
-
-index = key_index((unsigned char *)key, size);
 
 new = malloc(sizeof(hash_node_t *));
 if (!new)
 return (0);
 
+index = key_index((unsigned char *)key, size);
+
 new->value = strdup(value);
 new->key = strdup(key);
 new->next = NULL;
 
-if (ht->array[index] != NULL)
-{
-current = ht->array[index];
-while (current)
-{
-if (!strcmp(current->key, key))
-{
-current->value = strdup(value);
-return (1);
-}
-current = current->next;
-}
-new->next = ht->array[index];
-ht->array[index] = new;
-}
-else
-ht->array[index] = new;
+node_handler(ht, new, index);
 
 return (1);
 }
