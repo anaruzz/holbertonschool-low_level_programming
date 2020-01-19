2681ce7a63885a716668bc8b1605d9e0b25c0d0e
anaruzz <laabidigh@gmail.com>
2020-01-19 22:07:46 +0100

    Add a new value to a hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int index, size = ht->size;
-hash_node_t *new;
+hash_node_t *new, *current;
 
 if (!key || strlen(key) == 0 || !ht)
 return (0);
@@ -24,62 +24,25 @@ if (!new)
 return (0);
 
 new->value = strdup(value);
-new->key= strdup(key);
+new->key = strdup(key);
 new->next = NULL;
 
-if (ht->array[index])
+if (ht->array[index] != NULL)
 {
-  while(ht->array[index])
-  {
-    if (strcmp(ht->array[index]->key, key) != 0)
-    {
-      ht->array[index]->value = strdup(value);
-      return (1);
-    }
-    ht->array[index] = ht->array[index]->next;
-  }
-  new->next = ht->array[index];
-  ht->array[index] = new;
+current = ht->array[index]
+while (current)
+{
+if (!strcmp(current->key, key))
+{
+current->value = strdup(value);
+return (1);
+}
+current = current->next;
+}
+new->next = ht->array[index];
+ht->array[index] = new;
 }
 else
 ht->array[index] = new;
 return (1);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if ((int)index == -1)
-// {
-//   // ht->array[index] = malloc(sizeof(hash_node_t));
-//   // ht->array[index]->key = malloc(len + 1 * sizeof(char));
-//   // ht->array[index]->value = malloc(len + 1 * sizeof(char));
-//   // ht->array[index]->key = key;
-//   // ht->array[index]->value = value;
-//   // ht->array[index]->next = NULL
-//   // return (1);
-// }
-// else
-// {
-// ht->array[index] = malloc(sizeof(hash_node_t));
-// ht->array[index]->key = strdup(key);
-// ht->array[index]->value = strdup(value);
-// ht->array[index]->next = NULL;
-// return (1);
-// }
-//
-// return (1);
-// }
