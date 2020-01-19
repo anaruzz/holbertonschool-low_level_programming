a380a5a12df8de289bb3afd6318be3109e91135c
anaruzz <laabidigh@gmail.com>
2020-01-19 23:44:05 +0100

    Print a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -3,32 +3,30 @@
 #include <stdio.h>
 #include "hash_tables.h"
 /**
-* hash_table_get - add an element to the hash table
+* hash_table_print - print a hash table
 * @ht: hash table
-* @key: the string to add
-* Return: int
+* Return: No return
 */
 void hash_table_print(const hash_table_t *ht)
 {
-  unsigned int size = ht->size, i;
-  hash_node_t *tmp;
-  int flag = 0;
+unsigned int size = ht->size, i;
+hash_node_t *tmp;
+int flag = 0;
 
-if (!ht || !ht->array)
+if (!ht || !(ht->array))
 return;
 
-      printf("{");
-    for(i = 0; i < size - 1; i++)
-    {
-      tmp = ht->array[i];
-      if (tmp != NULL)
-      {
-        if (flag != 0)
-        printf(", ");
-        printf("'%s': '%s'", tmp->key, tmp->value);
-
-        flag = 1;
-      }
-    }
-    printf("}\n");
-  }
+printf("{");
+for (i = 0; i < size; i++)
+{
+tmp = ht->array[i];
+if (tmp != NULL)
+{
+if (flag != 0)
+printf(", ");
+printf("'%s': '%s'", tmp->key, tmp->value);
+flag = 1;
+}
+}
+printf("}\n");
+}
