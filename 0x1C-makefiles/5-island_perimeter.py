3cadd63aa4b9dbb14219e0c9a0b9d7e00209d043
anaruzz <laabidigh@gmail.com>
2020-02-21 22:54:07 +0100

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,7 +2,12 @@
 """
 5-island_perimeter.py
 """
+
+
 def island_perimeter(grid):
+    """
+    compute perimeter of island in a grid
+    """
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
